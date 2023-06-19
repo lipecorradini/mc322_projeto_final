5e738f2497be5080bcf556935d1cf37a4e61ae01
p8pablo <pabloalmeidamendes@hotmail.com>
2023-06-19 13:56:26 -0300

    reestruturação do projeto em java project

---
+++ projetoFinal/src/Main.java
@@ -0,0 +1,21 @@
+
+import javax.swing.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(() -> {
+      TelaHome telaHome = new TelaHome(
+        new JButton(),
+        new JFrame(),
+        false,
+        null,
+        new BarraNavegacao(),
+        false,
+        "null",
+        new JFrame()
+      );
+      telaHome.getFrameHome().setVisible(true);
+    });
+  }
+}
---
+++ projetoFinal/src/Tela.java
@@ -0,0 +1,81 @@
+
+import java.awt.*;
+
+import javax.swing.JFrame;
+
+public abstract class Tela {
+
+    public boolean mostrarBarraNavegacao;
+    public Color backgroundColor;
+    public BarraNavegacao barraNavegacao;
+    public boolean estaLogado;
+    public String titulo;
+    public JFrame frame;
+
+    public Tela(boolean mostrarBarraNavegacao, Color backgroundColor, BarraNavegacao barraNavegacao,
+            boolean estaLogado, String titulo, JFrame frame) {
+        this.mostrarBarraNavegacao = true;
+        this.backgroundColor = backgroundColor;
+        this.barraNavegacao = barraNavegacao;
+        this.estaLogado = estaLogado;
+        this.titulo = titulo;
+        this.frame = frame;
+    }
+
+    public boolean getMostrarBarraNavegacao() {
+        return mostrarBarraNavegacao;
+    }
+
+    public void setMostrarBarraNavegacao(boolean mostrarBarraNavegacao) {
+        this.mostrarBarraNavegacao = mostrarBarraNavegacao;
+    }
+
+    public Color getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(Color backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public BarraNavegacao getBarraNavegacao() {
+        return barraNavegacao;
+    }
+
+    public void setBarraNavegacao(BarraNavegacao barraNavegacao) {
+        this.barraNavegacao = barraNavegacao;
+    }
+
+    public boolean getEstaLogado() {
+        return estaLogado;
+    }
+
+    public void setEstaLogado(boolean estaLogado) {
+        this.estaLogado = estaLogado;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public boolean isMostrarBarraNavegacao() {
+        return this.mostrarBarraNavegacao;
+    }
+
+    public boolean isEstaLogado() {
+        return this.estaLogado;
+    }
+
+    public JFrame getFrame() {
+        return this.frame;
+    }
+
+    public void setFrame(JFrame frame) {
+        this.frame = frame;
+    }
+
+}
