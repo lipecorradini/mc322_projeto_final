50290534cb03139d7cdaef69d7a01a0259ef69b8
ra230613 <l230613@dac.unicamp.br>
2023-06-20 23:24:44 -0300

    layout tela carrinho iniciado

--- projetoFinal/src/Main.java
+++ projetoFinal/src/Main.java
@@ -5,21 +5,24 @@ public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             // TelaHome telaHome = new TelaHome(
-            //         false,
-            //         null,
-            //         new BarraNavegacao(),
-            //         false,
-            //         "null",
-            //         new JFrame());
+            // false,
+            // null,
+            // new BarraNavegacao(),
+            // false,
+            // "null",
+            // new JFrame());
             // configuracao frame exibicao
             JFrame frameExibicao = new JFrame();
-            frameExibicao.setSize(470, 930);
+            frameExibicao.setSize(390, 844);
             frameExibicao.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             // frameExibicao.add(telaHome.getLayeredPane());
             frameExibicao.setVisible(true);
 
             TelaLoginCadastro telaLoginCadastro = new TelaLoginCadastro(false, null);
-            frameExibicao.add(telaLoginCadastro.getPainelLoginCadastro());
+            // frameExibicao.add(telaLoginCadastro.getPainelLoginCadastro());
+
+            TelaCarrinho telaCarrinho = new TelaCarrinho(false, null);
+            frameExibicao.add(telaCarrinho.getLayeredPane());
         });
 
     }
