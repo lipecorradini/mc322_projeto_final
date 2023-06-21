1d73dc09df49fd63029e767c37623e40fc82b6f1
pedropiin <pedarosa04@gmail.com>
2023-06-20 23:16:34 -0300

    login finalizado esteticamente

--- projetoFinal/src/Main.java
+++ projetoFinal/src/Main.java
@@ -13,7 +13,7 @@ public class Main {
             //         new JFrame());
             // configuracao frame exibicao
             JFrame frameExibicao = new JFrame();
-            frameExibicao.setSize(470, 930);
+            frameExibicao.setSize(390, 844);
             frameExibicao.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             // frameExibicao.add(telaHome.getLayeredPane());
             frameExibicao.setVisible(true);
--- projetoFinal/src/TelaLoginCadastro.java
+++ projetoFinal/src/TelaLoginCadastro.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TelaLoginCadastro extends Tela {
+    public JLayeredPane paneCamadasLoginCadastro;
     public JPanel painelLoginCadastro;
     public JTextField textFieldUsuario;
     public JPasswordField passwordFieldSenha;
@@ -15,20 +16,28 @@ public class TelaLoginCadastro extends Tela {
                             Color backgroundColor) {
         super(mostrarBarraNavegacao, backgroundColor);
 
-        //Criando painel
+        paneCamadasLoginCadastro = new JLayeredPane();
+        paneCamadasLoginCadastro.setPreferredSize(new Dimension(390, 844));
+
+        //Criando painel login
         SpringLayout layout = new SpringLayout();
         painelLoginCadastro = new JPanel(layout);
+        painelLoginCadastro.setBackground(Color.white);
+        painelLoginCadastro.setBounds(100, 309, 500, 100);
+        painelLoginCadastro.setOpaque(false);
 
         //Criando textfields e labels para login e cadastro
         this.textFieldUsuario = new JTextField(10);
+        this.textFieldUsuario.setPreferredSize(new Dimension(100, 30));
         this.labelUsuario = new JLabel("Usuário: ");
         this.labelUsuario.setLabelFor(textFieldUsuario);
 
         this.passwordFieldSenha = new JPasswordField(10);
+        this.passwordFieldSenha.setPreferredSize(new Dimension(100, 30));
         this.labelSenha = new JLabel("Senha: ");
         this.labelSenha.setLabelFor(passwordFieldSenha);
 
-        //Adicionando labels no painel
+        // Adicionando labels no painel
         this.painelLoginCadastro.add(labelUsuario);
         this.painelLoginCadastro.add(textFieldUsuario);
         this.painelLoginCadastro.add(labelSenha);
@@ -37,28 +46,42 @@ public class TelaLoginCadastro extends Tela {
         //Settando Spring Layout
         String norte = SpringLayout.NORTH;
         String oeste = SpringLayout.WEST;
-        String centroHorizontal = SpringLayout.HORIZONTAL_CENTER;
-        String centroVertical = SpringLayout.VERTICAL_CENTER;
+        String leste = SpringLayout.EAST;
+        String sul = SpringLayout.SOUTH;
+        
+        layout.putConstraint(oeste, labelUsuario, 5, oeste, painelLoginCadastro);
+        layout.putConstraint(norte, labelUsuario, 7, norte, painelLoginCadastro);
+        layout.putConstraint(oeste, textFieldUsuario, 5, leste, labelUsuario);
+        layout.putConstraint(norte, textFieldUsuario, 2, norte, painelLoginCadastro);
 
-        layout.putConstraint(oeste, labelUsuario, -100, centroHorizontal, painelLoginCadastro);
-        layout.putConstraint(norte, labelUsuario, -80, centroVertical, painelLoginCadastro);
-        layout.putConstraint(oeste, textFieldUsuario,  -20, centroHorizontal, painelLoginCadastro);
-        layout.putConstraint(norte, textFieldUsuario, -80, centroVertical, painelLoginCadastro);
+        layout.putConstraint(oeste, labelSenha, 9, oeste, painelLoginCadastro);
+        layout.putConstraint(norte, labelSenha, 33, sul, labelUsuario);
+        layout.putConstraint(oeste, passwordFieldSenha, 12, leste, labelSenha);
+        layout.putConstraint(norte, passwordFieldSenha, 20, sul, textFieldUsuario);
 
-        layout.putConstraint(oeste, labelSenha, -100, centroHorizontal, painelLoginCadastro);
-        layout.putConstraint(norte, labelSenha, -40, centroVertical, painelLoginCadastro);
-        layout.putConstraint(oeste, passwordFieldSenha, -20, centroHorizontal, painelLoginCadastro);
-        layout.putConstraint(norte, passwordFieldSenha, -40, centroVertical, painelLoginCadastro);
+        //Importando e colocando imagem de fundo
+        ImageIcon imagemFundo = new ImageIcon("lib/Login.jpg");
+        JLabel labelImagemFundo = new JLabel(imagemFundo);
+        labelImagemFundo.setBounds(0, 0, 390, 844);
 
-        ImageIcon imagemFrase = new ImageIcon("../lib/frase_efeito_login_cadastro.jpeg");
-        ImageIcon imagemLogo = new ImageIcon("../lib/logo_loja.jpeg");
+        //Criando botões de login e cadastro e setando suas posições
+        ImageIcon iconImagemBotaoCadastro = new ImageIcon("lib/botaoCadastrar.jpg");
+        ImageIcon iconImagemBotaoLogin = new ImageIcon("lib/botaoLogin.jpg");
+        botaoCadastro = new JButton(iconImagemBotaoCadastro);
+        botaoLogin = new JButton(iconImagemBotaoLogin);
+        botaoCadastro.setBorderPainted(false);
+        botaoLogin.setBorderPainted(false);
+        botaoCadastro.setBounds(123, 454, 143, 47);
+        botaoLogin.setBounds(123, 520, 143, 47);
 
-        JLabel imagem1 = new JLabel(imagemFrase);
-        this.painelLoginCadastro.add(imagem1);
-
+        //Adicionando as camadas ao pane
+        paneCamadasLoginCadastro.add(labelImagemFundo, Integer.valueOf(0));
+        paneCamadasLoginCadastro.add(painelLoginCadastro, Integer.valueOf(1));
+        paneCamadasLoginCadastro.add(botaoCadastro, Integer.valueOf(2));
+        paneCamadasLoginCadastro.add(botaoLogin, Integer.valueOf(3));
     }
 
-    public JPanel getPainelLoginCadastro() {
-        return this.painelLoginCadastro;
+    public JLayeredPane getPainelLoginCadastro() {
+        return this.paneCamadasLoginCadastro;
     }
 }
